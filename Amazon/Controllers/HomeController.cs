196f3ed1f9db05cc6bd516eeb17fbe7149873efa
cairomurphy <84107491+cairomurphy@users.noreply.github.com>
2022-02-25 15:56:46 -0700

    backup

---
+++ Amazon/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amazon.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookstoreContext context { get; set; }
+
+        public HomeController(BookstoreContext temp) => context = temp;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
