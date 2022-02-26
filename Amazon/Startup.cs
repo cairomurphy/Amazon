0c128d701941925b2080797aca4be73a27127e38
cairomurphy <84107491+cairomurphy@users.noreply.github.com>
2022-02-25 22:47:37 -0700

    amazon update 2

--- Amazon/Startup.cs
+++ Amazon/Startup.cs
@@ -32,6 +32,8 @@ namespace Amazon
             });
 
             services.AddScoped<IBookStoreRepository, EFBookStoreRepository>();
+
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +64,8 @@ namespace Amazon
                     new { Controller = "Home", action = "Index", pageNum = 1 });
 
                 endpoints.MapDefaultControllerRoute();
+
+                endpoints.MapRazorPages();
             });
         }
     }
