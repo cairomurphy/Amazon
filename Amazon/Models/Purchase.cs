690247fb2c9db24032a756bbb0ebcaff10cd35c5
cairomurphy <84107491+cairomurphy@users.noreply.github.com>
2022-03-03 00:17:44 -0700

    commit

---
+++ Amazon/Models/Purchase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amazon.Models
+{
+    public class Purchase
+    {
+        [Key]
+        [BindNever]
+        public int PurchaseId { get; set; }
+
+        [BindNever]
+        public ICollection<BasketLineItem> Lines { get; set; }
+        [Required(ErrorMessage = "Please enter a name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter the first line")]
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        [Required(ErrorMessage = "Please enter the a city name")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Please enter the a state")]
+        public string State { get; set; }
+        public string Zip { get; set; }
+        [Required(ErrorMessage = "Please enter the a country")]
+        public string Country { get; set; }
+        public bool Gift { get; set; }
+    }
+}
